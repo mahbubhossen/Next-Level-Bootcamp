5b2b02cef697d584fb465c693d8665401c3b2ab7
Mahbub Hossen <mahbubhossenpg@gmail.com>
2026-01-18 12:48:18 +0600

    complete module 3

--- Mission 1/Module 3/vdo-7.js
+++ Mission 1/Module 3/vdo-7.js
@@ -25,6 +25,7 @@ class LinkedList {
         this.tail = newNode;
     }
     this.length++;
+    return this; // evabe return this likhle amra linkedlist.append(0).append(1).append(3); evabe likhte parbo. 
   }
   prepend(value) {
     const newNode = new Node(value);
@@ -39,6 +40,7 @@ class LinkedList {
      this.head = newNode;
     }
     this.length++;
+    return this; 
   }
   insert(index, value) {
     if(index < 0 || index > this.length) {
@@ -58,16 +60,51 @@ class LinkedList {
 
     // if the insert is in the middle 
     //step-1: find the leading node
-    let count = 0;
-    let leadingNode = this.head;
+    const leadingNode = this._traverseToIndex(index-1); // leading node : target node er ager node tai leading node . 
+    const holdingNode = leadingNode.next;
 
-    while(count !== index-1){
-      leadingNode = leadingNode.next;
-      count++;
-    }
+    const newNode = new Node(value);
+
+    leadingNode.next = newNode;
+    newNode.next = holdingNode;
+    this.length++;
     
   }
-  remove() {}
+  remove(index) {
+    if(index === 0){
+      const removedHead = this.head.value;
+      this.head = this.head.next;
+
+      if(this.length === 1){
+        this.tail = null;
+      }
+
+      this.length--;
+      return removedHead; 
+    }
+
+    const leadingNode = this._traverseToIndex(index-1);
+    const nodeToRemove = leadingNode.next;
+
+    leadingNode.next = nodeToRemove.next;
+
+    if(leadingNode.next === null){
+      this.tail = leadingNode;
+    }
+
+    return nodeToRemove.value;
+  }
+
+  _traverseToIndex(index){ // ekhane underscore _ dewa mane holo eti private function . 
+    let count = 0;
+    let currentNode = this.head;
+
+    while (count !== index ) {
+      currentNode = currentNode.next;
+      count++;
+    }
+    return currentNode;
+  }
   print() {
     const arr = [];
     let currentNode = this.head;
@@ -81,12 +118,40 @@ class LinkedList {
 }
 
 const linkedList = new LinkedList();
-linkedList.append(1);
-linkedList.append(2);
-linkedList.append(3);
+// linkedList.append(1);
+// linkedList.append(2);
+// linkedList.append(3);
 
-linkedList.prepend(10);
-linkedList.prepend(20);
-linkedList.prepend(30);
+// linkedList.prepend(10);
+// linkedList.prepend(20);
+// linkedList.prepend(30);
 
+// linkedList.append(0);
+// linkedList.append(1);
+// linkedList.append(3);
+
+// linkedList.append(0).append(1).append(3); // return this likhar karone evabe likhte partesi . 
+
+// linkedList.insert(2, 2);
+
+// linkedList.append("A"); //*0
+// linkedList.append("B"); //*1
+// // linkedList.append("C"); //*2
+// linkedList.append("D"); //*3
+
+// linkedList.print();
+
+// linkedList.remove(2); // remove form middle
+// linkedList.print();
+
+// linkedList.remove(3); // remove from last
+// linkedList.print();
+
+// linkedList.remove(2);
+// linkedList.remove(0); // remove from first 
+// linkedList.print();
+
+
+linkedList.append("A"); //*0
+linkedList.remove(0);
 linkedList.print();
