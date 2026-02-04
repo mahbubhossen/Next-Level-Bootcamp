4e9f25b8730c82374c3f392d1016d3c5682aa1bb
Mahbub Hossen <mahbubhossenpg@gmail.com>
2026-02-05 05:24:58 +0600

    module 12

---
+++ Mission 3/Module 12/README.md
@@ -0,0 +1,29 @@
+# Module 12 :
+
+express js : framework of node js
+
+at first npm init -y
+then npm install express --save
+npm i -D typescript
+npx tsc --init
+then : "rootDir": "./src",
+"outDir": "./dist",
+uncomment these two lines in tsconfig.json file
+
+comment this :
+// "sourceMap": true,
+// "declaration": true,
+// "declarationMap": true,
+// "jsx": "react-jsx",
+// "verbatimModuleSyntax": true,
+
+create src folder and create server.ts file
+then paste which copy from express file 
+
+then install  : npm i --save-dev @types/express
+
+npm i -D tsx  for : auto load server
+
+npm i pg  for : connection with postgresql
+
+npm i dotenv
---
+++ Mission 3/Module 12/src/server.ts
@@ -0,0 +1,62 @@
+import dotenv from "dotenv";
+import express, { Request, Response } from "express";
+import path from "path";
+import { Pool } from "pg";
+
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+const app = express();
+const port = 5000;
+
+const pool = new Pool({
+  connectionString: `${process.env.CONNECTION_STR}`,
+});
+
+const initDB = async () => {
+  await pool.query(`
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(100) NOT NULL,
+            email VARCHAR(100) UNIQUE NOT NULL,
+            age INT,
+            phone VARCHAR(15),
+            address TEXT,
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+        )
+        `);
+  await pool.query(`
+            CREATE TABLE IF NOT EXISTS todos(
+            id SERIAL PRIMARY KEY,
+            user_id INT REFERENCES users(id) ON DELETE CASCADE,
+            title VARCHAR(200) NOT NULL,
+            description TEXT,
+            completed BOOLEAN DEFAULT false,
+            due_date DATE,
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+            )
+            `);
+};
+
+initDB();
+
+//parser
+app.use(express.json()); // for parse json data
+// app.use(express.urlencoded());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello next level developers !");
+});
+
+app.post("/", (req: Request, res: Response) => {
+  console.log(req.body);
+  res.status(201).json({
+    success: true,
+    message: "Api is working",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
